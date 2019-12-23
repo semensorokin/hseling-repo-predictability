ac1a8c11e2befd77abe2b6ba62e50f7c73057146
Sergey Sobko <ssobko@hse.ru>
2019-12-23 04:40:27 +0300

    Initial commit

---
+++ hseling_api_predictability/main.py
@@ -0,0 +1,3 @@
+from hseling_api_predictability.main import (
+    app,
+)
---
+++ hseling_api_predictability/tests/test_hseling_api_predictability.py
@@ -0,0 +1,17 @@
+import unittest
+
+import hseling_api_predictability
+
+
+class HSELing_API_PredictabilityTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = hseling_api_predictability.app.test_client()
+
+    def test_index(self):
+        rv = self.app.get('/healthz')
+        self.assertIn('Application Predictability', rv.data.decode())
+
+
+if __name__ == '__main__':
+    unittest.main()
